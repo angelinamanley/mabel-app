86f8a679e7f1dd63f453402f1acf5526bdf37c21
github username <github email address>
2019-09-09 11:23:25 +0100

    seed date added, pet show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_084617) do
+ActiveRecord::Schema.define(version: 2019_09_09_100102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_084617) do
     t.date "dob"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "species"
     t.index ["owner_id"], name: "index_pets_on_owner_id"
   end
 
